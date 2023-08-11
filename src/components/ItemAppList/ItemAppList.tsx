2324f8d0b3e4c3cd3098ab20cf9593913f09924c
Maxim <Sen989@yandex.ru>
2023-08-11 13:13:46 +0700

    change itemApp

--- src/components/ItemAppList/ItemAppList.tsx
+++ src/components/ItemAppList/ItemAppList.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+
+import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { setSingleRepoData } from '../../redux/slice/datasSlice';
 import './ItemAppList.scss';
